2ec05045cf9d11c16749af9bb9056bcb6ea1f7d1
sylwiagutowska <syl.gutowska@gmail.com>
2020-10-06 22:53:03 +0200

    making kgrid almost works

--- elph_on_FS.py
+++ elph_on_FS.py
@@ -1,27 +1,65 @@
 dir='tmp_dir/_ph0/ir.phsave/'
 import xml.etree.ElementTree as ET
+import numpy as np
+from operator import itemgetter
 
-###read energies
+PRECIS=3
+def sorting(allk2):
+ Xall=[]
+ allk2=sorted(allk2, key=itemgetter(0))
+ i=0
+ while i<len(allk2): 
+  X=[]
+  x=allk2[i]
+  while i<len(allk2) and x[0]==allk2[i][0]:
+   X.append(allk2[i])
+   i=i+1
+  if len(X)>1: X=sorted(X, key=itemgetter(1))
+  Xall.append(X)
+
+ Yall=[]
+ for X in Xall:
+  x=X[0]
+  i=0
+  while i<len(X): 
+   Y=[]
+   x=X[i]
+   while i<len(X) and x[1]==X[i][1]:
+    Y.append(X[i])
+    i=i+1
+   Y=sorted(Y, key=itemgetter(2))
+   Yall.append(Y)
+
+ allk=[]
+ for i in Yall:
+  for j in i:
+   allk.append(j)
+
+ print ' Sorting - Done!'
+
+ return allk
+
+###read energies and other electronic and structure info
 Ha_to_ev=13.605662285137*2
 #file=raw_input('Path and name of *.xml file, eq. Pb or tmp_dir/Pb: ')
 tree = ET.parse('FS/tmp_dir/ir.xml')
 root = tree.getroot()
 ENE=[]
 NONEQ=[]
-#for child in root:
-#     print child.tag, child.attrib
+
 for i in root.findall('output/band_structure/fermi_energy'):
  ef=float(i.text.split()[0])
 print(ef)
+
+for i in root.findall('output/band_structure/starting_k_points/monkhorst_pack'):
+ no_of_kpoints=[ int(i.get('nk1')),int(i.get('nk2')),int(i.get('nk3'))]
+print( no_of_kpoints)
 
 for i in root.findall('output/band_structure/ks_energies'):
     for actor in i.findall('eigenvalues'):
      ENE.append([float(m) for m in  actor.text.split()])
     for actor in i.findall('k_point'):
-     NONEQ.append([float(m) for m in  actor.text.split()])
-
-print (len(ENE))
-print (len(NONEQ))
+     NONEQ.append([round(float(m),PRECIS) for m in  actor.text.split()])
 
 maks=max([ len(i) for i in ENE])
 ENE2= [ [] for i in range(maks)]
@@ -33,22 +71,139 @@ for i in ENE:
   ENE2[numj].append(j)
   if j<ef: below_ENE[numj]=1
   elif j>ef: top_ENE[numj]=1
-
-#print('\nFermi level='+str(ef*Ha_to_ev))
-
-#print('\nList of bands and its energy ranges:')
-#for i in range(maks):
-# print i+1, min(ENE2[i])*Ha_to_ev, max(ENE2[i])*Ha_to_ev
-
 bands_num=[]
 #print('\nList of bands, which cross the Fermi level and its energy ranges:')
 for i in range(maks):
  if below_ENE[i] and top_ENE[i]: 
   bands_num.append(i)
 minband,maxband=bands_num[0],bands_num[-1]
-#  print i+1, min(ENE2[i])*Ha_to_ev, max(ENE2[i])*Ha_to_ev
+
+#reciprocal lattice vectors
+e=[]
+for i in root.findall('output/basis_set/reciprocal_lattice'):
+ e.append([round(float(m),PRECIS) for m in i.find('b1').text.split()])
+ e.append([round(float(m),PRECIS) for m in i.find('b2').text.split()])
+ e.append([round(float(m),PRECIS) for m in i.find('b3').text.split()])
+e=np.array(e)
+print e
+
+#symmetry operations
+SYMM=[np.array([[1,0,0],[0,1,0],[0,0,1]])]
+for neighbor in root.iter('rotation'):
+     tmp=neighbor.text.split()
+     tmp2=np.array([ [ float(m) for m in tmp[0:3]], [float(m) for m in tmp[3:6]], [float(m) for m in tmp[6:9]]])
+     SYMM.append(np.dot((np.linalg.inv(e)),(np.dot(tmp2,e))))
+print len(SYMM)
 ### 
 
+
+
+###make whole kgrid
+
+allk=[]
+mmm=0
+pm=[-1,0.,1,-2,2]
+A_vectors=[np.cross(e[0],e[1]),np.cross(e[2],e[0]),np.cross(e[1],e[2]),\
+           -np.cross(e[0],e[1]),-np.cross(e[2],e[0]),-np.cross(e[1],e[2])]
+B_vectors=[e[2]+0.5*(e[0]+e[1]),e[1]+0.5*(e[0]+e[2]),e[0]+0.5*(e[1]+e[2]),\
+0.5*(e[0]+e[1]),0.5*(e[0]+e[2]),0.5*(e[1]+e[2]) ]
+A_vectors=[ [round(kk,PRECIS+2) for kk in m] for m in A_vectors]
+B_vectors=[ [round(kk,PRECIS+2) for kk in m] for m in B_vectors]
+
+#NONEQ=[ [ round(sum([v[m]*np.transpose((e))[m2][m] for m in range(3)]),4) for m2 in range(3)] for v in NONEQ]
+NONEQ2=[]
+
+#trans
+
+for nq in NONEQ:
+   for h1 in pm:
+    for k1 in pm:
+     for l1 in pm:
+      k_point2=nq+(h1*e[0]+k1*e[1]+l1*e[2]) 
+      sign0=0
+      for numa,a in enumerate(A_vectors):
+  #    if k_point2[0]*B_vectors[numa][0]+ k_point2[1]*B_vectors[numa][1]+k_point2[2]*B_vectors[numa][2]>=0:
+        ra=k_point2-B_vectors[numa]
+        scalar_product=sum([a[i]*ra[i] for i in range(3)])
+        if scalar_product>0 or (numa<3 and scalar_product==0): 
+         sign0=1
+         break
+      if sign0==0:
+        NONEQ2.append([round(kk,PRECIS) for kk in k_point2])
+
+
+NONEQ2=sorting(NONEQ2)
+
+NONEQ=[] 
+for i in range(len(NONEQ2)-1):
+  x=NONEQ2[i]
+  y=NONEQ2[i+1]
+  if not(x[0]==y[0] and x[1]==y[1] and x[2]==y[2]):
+   NONEQ.append(x)
+print len(NONEQ)
+
+
+for nq in NONEQ:
+ # print nq
+  for sym in SYMM:
+   x=[sum([sym[m1][m2]*nq[m2] for m2 in range(3)]) for m1 in range(3)]
+   #if x[0]<maxe[0] and x[1]<maxe[1] and x[2]<maxe[2] and x[0]>-maxe[0] and x[1]>-maxe[1] and x[2]>-maxe[2]:
+  # allk.append([round(kk,PRECIS) for kk in x])
+   for h1 in pm:
+    for k1 in pm:
+     for l1 in pm:
+      k_point2=x-(h1*e[0]+k1*e[1]+l1*e[2]) 
+      sign0=0
+      for numa,a in enumerate(A_vectors):
+     #  if k_point2[0]*a[0]>=0 and k_point2[1]*a[1]>=0 and k_point2[2]*a[2]>=0:
+        ra=k_point2-B_vectors[numa]
+        scalar_product=sum([a[i]*ra[i] for i in range(3)])
+        if scalar_product>0  or (numa<3 and scalar_product==0): 
+         sign0=1
+         break
+      if sign0==0:
+        allk.append([round(kk,PRECIS) for kk in k_point2])
+
+
+print len(allk)
+allk2=sorting(allk)
+
+allk=[] 
+for i in range(len(allk2)-1):
+  x=allk2[i]
+  y=allk2[i+1]
+  if not(x[0]==y[0] and x[1]==y[1] and x[2]==y[2]):
+   allk.append(x)
+print len(allk)
+
+
+
+h=open('kpoints.dat','w')
+for i in allk:
+ for j in i:
+  h.write(str(j)+' ')
+ h.write('\n')
+h.close()
+
+allk=[ [ round(sum([v[m]*np.linalg.inv(np.transpose(e))[m2][m] for m in range(3)]),4) for m2 in range(3)] for v in allk]
+allk=sorting(allk)
+
+
+h=open('kpoints0.dat','w')
+for i in allk:
+ for j in i:
+  h.write(str(j)+' ')
+ h.write('\n')
+h.close()
+
+h=open('kpoints_noneq.dat','w')
+for i in NONEQ:
+ for j in i:
+  h.write(str(j)+' ')
+ h.write('\n')
+h.close()
+
+###
 
 ###read frequencies and make DEG[q][nbnd] = no of band, with whom the nbnd is degenerated
 h=open('ir.dyn1')
